795d97bd9fa476c0e4d0f6fb5e209afc5152177b
xoni.xxt <jgabrielwrk@gmail.com>
2025-08-01 22:44:50 -0300

    Add Member class with validation methods

--- Member.java
+++ Member.java
@@ -7,17 +7,42 @@ public class Member {
     private final String phone;
     private boolean active;
 
-    public Member(String name, int age, String email, String phone){
-        if (name == null || name.isBlank()){
-            throw new IllegalArgumentException("You must insert a valid name.");
-        }
+    public Member(String name, int age, String email, String phone) {
+        validateName(name);
+        validateAge(age);
+        validateEmail(email);
+        validatePhone(phone);
         this.name = name;
-
         this.age = age;
         this.email = email;
         this.phone = phone;
         this.id = ++nextId;
         this.active = false;
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("You must insert a valid name.");
+        }
+    }
+
+    private void validateAge(int age) {
+        if (age <= 0) {
+            throw new IllegalArgumentException("Insert your true age.");
+        }
+    }
+
+    // Improve
+    private void validateEmail(String email) {
+        if (email == null || !email.contains("@")) {
+            throw new IllegalArgumentException("Insert a valid email.");
+        }
+    }
+
+    private void validatePhone(String phone) {
+        if (phone == null || !phone.matches("\\d{11}")) {
+            throw new IllegalArgumentException("Insert your number with DDD and without space or symbol");
+        }
     }
 
     public int getId() {
