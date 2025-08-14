25de5cb1480bee8c17f5ac43b6f23b2de4cb9415
xoni.xxt <jgabrielwrk@gmail.com>
2025-08-13 22:11:54 -0300

    Função de adicionar ao BD

--- ConexaoMySql.java
+++ ConexaoMySql.java
@@ -1,18 +1,27 @@
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
 
-public class ConexaoMySql {
-private static final String URL = "jdbc:mysql://localhost:3306/corpoemeta";
-private static final String USER = "root";
-private static final String PASSWORD = "root";
+public class ConexaoMySql{
+    private static final String URL = "jdbc:mysql://localhost:3306/corpoemeta";
+    private static final String USER = "root";
+    private static final String PASS = "root";
 
-public void testandoConexao(){
-    try(Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)){
-        System.out.println("Conexão estabelecida com sucesso.");
+    public void adicionarBD(Pessoa pessoa){
+        String sql = "INSERT INTO Pessoa (nome, idade, email, telefone, ativo) VALUES (?, ?, ?, ?, ?)";
+        try(Connection conn = DriverManager.getConnection(URL, USER, PASS);
+        PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setString(1, pessoa.getNome());
+            stmt.setInt(2, pessoa.getIdade());
+            stmt.setString(3, pessoa.getEmail());
+            stmt.setString(4, pessoa.getTelefone());
+            stmt.setBoolean(5, pessoa.estaAtivo());
+
+            stmt.executeUpdate();
+            System.out.println("Pessoa adicionada com sucesso ao Banco de Dados.");
+        }catch(SQLException exception){
+            System.out.println("Erro ao adicionar pessoa: " + exception.getMessage());
+        }
     }
-    catch (SQLException exception){
-        System.out.println("Erro ao estabelecer conexão: " + exception.getMessage());
-    }
-}
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa("Johnny", 19, "jgabrielwrk@gmail.com", "83982235507");
         ConexaoMySql conexao = new ConexaoMySql();
-        conexao.testandoConexao();
+        conexao.adicionarBD(pessoa1);
     }
 }
