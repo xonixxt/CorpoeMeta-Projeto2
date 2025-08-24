78aceb69e00903f410a32a5a669f4911e6b9aa7f
xoni.xxt <jgabrielwrk@gmail.com>
2025-08-24 18:42:57 -0300

    Método para adicionar Pessoa finalizado

--- ConexaoMySql.java
+++ ConexaoMySql.java
@@ -1,27 +1,55 @@
-import java.sql.DriverManager;
 import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-public class ConexaoMySql{
+public class ConexaoMySql {
     private static final String URL = "jdbc:mysql://localhost:3306/corpoemeta";
     private static final String USER = "root";
     private static final String PASS = "root";
 
-    public void adicionarBD(Pessoa pessoa){
-        String sql = "INSERT INTO Pessoa (nome, idade, email, telefone, ativo) VALUES (?, ?, ?, ?, ?)";
-        try(Connection conn = DriverManager.getConnection(URL, USER, PASS);
-        PreparedStatement stmt = conn.prepareStatement(sql)){
+    public void adicionarPessoaBD(Pessoa pessoa) {
+        String sql = "INSERT INTO Pessoa(nome, idade, email, telefone, ativo) VALUES (?, ?, ?, ?, ?)";
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASS);
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setString(1, pessoa.getNome());
             stmt.setInt(2, pessoa.getIdade());
             stmt.setString(3, pessoa.getEmail());
             stmt.setString(4, pessoa.getTelefone());
             stmt.setBoolean(5, pessoa.estaAtivo());
-
             stmt.executeUpdate();
-            System.out.println("Pessoa adicionada com sucesso ao Banco de Dados.");
-        }catch(SQLException exception){
-            System.out.println("Erro ao adicionar pessoa: " + exception.getMessage());
+            System.out.println("Pessoa adicionada com sucesso ao BD.");
+        } catch (SQLException e) {
+            System.out.println("Erro ao adicionar pessoa: " + e.getMessage());
+        }
+    }
+
+    public void atualizarPessoaBD(Pessoa pessoa) {
+        String sql = "UPDATE Pessoa SET nome = ?, idade = ?, email = ?, telefone = ?, ativo = ? WHERE id = ?";
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASS);
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, pessoa.getNome());
+            stmt.setInt(2, pessoa.getIdade());
+            stmt.setString(3, pessoa.getEmail());
+            stmt.setString(4, pessoa.getTelefone());
+            stmt.setBoolean(5, pessoa.estaAtivo());
+            stmt.setInt(6, pessoa.getId());
+            stmt.executeUpdate();
+            System.out.println("Atualização executada com sucesso no BD.");
+        } catch (SQLException e) {
+            System.out.println("Erro ao atualizar pessoa: " + e.getMessage());
+        }
+    }
+
+    public void removerPessoaBD(int id) {
+        String sql = "DELETE FROM Pessoa WHERE id = ?";
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASS);
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+            System.out.println("Pessoa removida com sucesso no BD.");
+        } catch (SQLException e) {
+            System.out.println("Erro ao remover pessoa: " + e.getMessage());
         }
     }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Pessoa pessoa1 = new Pessoa("Johnny", 19, "jgabrielwrk@gmail.com", "83982235507");
+        Pessoa p1 = new Pessoa("Johnny", 19, "jgabrielwrk@gmail.com", "83982235507");
         ConexaoMySql conexao = new ConexaoMySql();
-        conexao.adicionarBD(pessoa1);
+
+        conexao.adicionarPessoaBD(p1);
     }
 }
